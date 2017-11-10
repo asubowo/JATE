f903795f4b8c461961072c0593faa6d8a4cb8cc0
Andrew Subowo <andrew.subowo@gmail.com>
2017-11-09 23:50:32 -0500

    Stabilized save function and added some eye candy

--- app/src/main/java/com/andrewsubowo/justanothertexteditor/io/SaveFile.java
+++ app/src/main/java/com/andrewsubowo/justanothertexteditor/io/SaveFile.java
@@ -18,6 +18,10 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
 import java.io.Writer;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 
 /**
  * Created by asubowo on 9/2/2017.
@@ -29,17 +33,22 @@ public class SaveFile extends AsyncTask<Void, Void, Boolean> {
     private Editor editor;
     private String filen;
     private File path, directory, file;
+    private boolean quit;
+
 
     /**
      * Constructor class for the SaveFile class
      *
      * @param editorActivity - Pass in the EditorActivity to this class
      * @param editor         - Pass in the Editor class
+     * @param filename       - Pass in the filename
+     *
      */
     public SaveFile(EditorActivity editorActivity, Editor editor, String filename) {
         this.editorActivity = editorActivity;
         this.editor = editor;
         this.filen = filename;
+        this.quit = quit;
         System.out.println(editorActivity.getApplicationContext().getFilesDir().toURI().toString());
 
         path = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOCUMENTS);
@@ -53,6 +62,11 @@ public class SaveFile extends AsyncTask<Void, Void, Boolean> {
         Snackbar.make(view, "Saving file...", Snackbar.LENGTH_LONG)
                 .show();
 
+
+        // If the directory to save the file doesn't exist, create it
+        if (!directory.exists()) {
+            directory.mkdirs();
+        }
     }
 
     @Override
@@ -86,11 +100,19 @@ public class SaveFile extends AsyncTask<Void, Void, Boolean> {
         if (result) {
             Snackbar.make(view, filen + " has been saved", Snackbar.LENGTH_LONG)
                     .show();
+            editorActivity.setSaveStatus(true);
             editorActivity.setNewTitle(filen);
+            Date date = Calendar.getInstance().getTime();
+            DateFormat df = new SimpleDateFormat("hh:mm:ss");
+
+            editorActivity.setSubtitle("Saved " + df.format(date));
         } else {
             Snackbar.make(view, "The file was unable to be saved", Snackbar.LENGTH_LONG)
                     .show();
+            editorActivity.setSaveStatus(false);
         }
+
 
     }
 }
+
